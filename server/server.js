0dd8a72f4199a6dcec5971ce32d05fe16c482062
omer-zauber <omer.zauber@gmail.com>
2018-01-31 12:13:36 +0200

    add connect and disconnect messages

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 //const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-const app = new express();
-const port = process.env.PORT || 3000;
 //const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = new express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+  });
+});
+
+server.listen(port, () => {
   console.log(`server is up at port: ${port}`);
-})
+});
 
 
 
