a097749c5b61e9d2328deecac7734f6536dd89d8
omer-zauber <omer.zauber@gmail.com>
2018-02-01 16:17:03 +0200

    scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //heights
+  var clientHeight = messages.prop('clientHeight'); 
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop +newMessageHeight +lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', function () {
   console.log('connected to server');
 });
@@ -14,6 +30,7 @@ socket.on('newMessage', function (message) {
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -25,6 +42,7 @@ socket.on('newLocationMessage', function (message) {
     createdAt: formatedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 var messageTextbox = jQuery('[name=message]');
